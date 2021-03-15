236672aa3c448414bff0d91c54c3932710e3487f
AlejandroOrtega17 <00166120@uca.edu.sv>
2021-03-15 11:19:35 -0600

    The first ended function

---
+++ GitHubWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GitHubWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a= 0, b= 0;
+
+            Console.WriteLine("Ingrese un valor para a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+             Console.WriteLine("Ingrese un valor para b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La suma de {a} + {b} es igual a {Addnumbers(a,b) }");
+        }
+
+        public static int Addnumbers(int a, int b)
+        {
+
+            return a + b;
+        }
+
+    }
+}
